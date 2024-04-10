a1417d34d1a62166cdeb4cd80a75a014da65fab0
kimYong91 <159098694+kimYong91@users.noreply.github.com>
2024-04-10 22:10:08 +0900

    서브쿼리 연습 문제 7번 질문

--- 메인쿼리, 서브쿼리/연습문제/서브쿼리 연습문제 2.sql
+++ 메인쿼리, 서브쿼리/연습문제/서브쿼리 연습문제 2.sql
@@ -19,16 +19,28 @@ SELECT C.first_name, C.last_name, COUNT(R.rental_id) FROM customer C
 			HAVING COUNT(R.rental_id) IN (SELECT COUNT(rental_id) FROM rental GROUP BY customer_id HAVING COUNT(rental_id) > 5);
 -- 문제3. 5편 이상의 영화에 출연한 모든 배우의 이름과 성을 조회하세요.
 -- 사용 테이블: actor (actor_id, first_name, last_name), film_actor (actor_id, film_id)
+SELECT actor_id FROM film_actor GROUP BY actor_id HAVING COUNT(film_id) >= 5;
 
+SELECT first_name, last_name FROM actor 
+	WHERE actor_id IN (SELECT actor_id FROM film_actor GROUP BY actor_id HAVING COUNT(film_id) >= 5);
 
 -- 문제4. 평균 영화 길이보다 긴 모든 영화의 제목을 조회하세요.
 -- 사용 테이블: film (film_id, title, length)
+SELECT AVG(length) FROM film;
 
+SELECT title, length FROM film WHERE length > (SELECT AVG(length) FROM film);
 -- 문제5. 평균 대여 비용보다 높은 대여 비용을 가진 모든 영화의 제목과 대여 비용을 조회하세요.
 -- 사용 테이블: film (film_id, title, rental_rate)
+SELECT AVG(rental_rate) FROM film;
 
+SELECT title, rental_rate FROM film WHERE rental_rate > (SELECT AVG(rental_rate) FROM film);
 -- 문제6. 각 영화에 출연한 배우의 수와 함께 영화 제목을 조회하세요. (SELECT절 사용)
 -- 사용 테이블: film (film_id, title), film_actor (actor_id, film_id)
+SELECT COUNT(actor_id) FROM film_actor GROUP BY film_id;
 
+SELECT title, (SELECT COUNT(actor_id) FROM film_actor FA WHERE FA.film_id = F.film_id) FROM film F;
 -- 문제7. 단일 고객이 가장 많이 대여한 영화 수를 조회하세요. (FROM절 사용)
 -- 사용 테이블: rental (rental_id, customer_id)
+SELECT COUNT(rental_id) FROM rental GROUP BY customer_id;
+
+SELECT MAX(RI) FROM (SELECT COUNT(rental_id) RI FROM rental GROUP BY customer_id) R;
